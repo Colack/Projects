4235734cca349e3dea7ee19db5047880d4e14dfb
Jack Spencer <UniformKitten281@gmail.com>
2022-05-23 13:41:59 -0700

    Create index.ts

---
+++ src/insertTextHere-Rework/index.ts
@@ -0,0 +1,26 @@
+//! usr/bin/env node 
+
+var checkpoint : number = 0;
+var magicUnlock : boolean = false;
+
+var strength : number = 0;
+var resistance : number = 0;
+var magic : number = 0;
+var agility : number = 0;
+var language : number = 0;
+var hitPoints : number = 0;
+
+function onGameStart() {
+
+    print("Welcome to [insert text here]!");
+    print("Unknown: Hello Player...");
+
+}
+
+function print(message : string) {
+
+    console.log(message);
+
+}
+
+onGameStart();
