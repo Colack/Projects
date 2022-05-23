3c6b4896dc7b1f960a19e5572d33c86b6f38ba16
Jack Spencer <UniformKitten281@gmail.com>
2022-05-23 13:03:52 -0700

    Create index.js

---
+++ src/Logger/index.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+
+// Create a new File that has a chosen log, plus some extra features.
+export function createLog(code, message) {
+ 	
+ 	// Pull a random Number
+  var x = getRandomInt();
+  
+  // Create
+  
+	// Create content of the File
+
+
+	// Create a random number
+  function getRandomInt() {
+  	return Math.floor(Math.random() * 3);
+	}
+}
