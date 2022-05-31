42c25bd04650c8096fe70dcb3e26f0c80a58adf2
Jack Spencer <UniformKitten281@gmail.com>
2022-05-31 08:21:09 -0700

    Delete src/Gold/src/installers directory

--- src/Gold/src/installers/current/main.ps1
+++
@@ -1,9 +0,0 @@
-$version="Alpha 1.1.2"
-
-For ($i=0; $i -le 100; $i++) {
-    Start-Sleep -Milliseconds 20
-    Write-Progress -Activity "Installing..." -PercentComplete $i -CurrentOperation "Version: $version"
-    mkdir bin >$null 2>&1
-    git clone https://github.com/Shining-Gold-Studios/Gold-Installer-Repo.git . >$null 2>&1
-
-}   
