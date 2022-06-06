4366fe1ac1d5e39c0a7420ff7362eec33aed4c65
Jack Spencer <UniformKitten281@gmail.com>
2022-06-06 10:26:24 -0700

    Update main.c

--- src/P-Grab/src/C-Build/main.c
+++ src/P-Grab/src/C-Build/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAX_LIMIT 20
 
-main() 
+void slashN() {
+    printf("\n");
+}
+
+int main() 
 {
-  
+    char str[MAX_LIMIT];
+    printf("Enter Username: ");
+    fgets(str, MAX_LIMIT, stdin);
+    printf("https://github.com/%s.png?size=1000", str);
+
+    return 0;
 }
