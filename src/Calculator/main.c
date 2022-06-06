13adafd11426b1b8766cc93e1753ab2451edede9
Jack Spencer <UniformKitten281@gmail.com>
2022-06-06 10:55:20 -0700

    Update main.c

--- src/Calculator/main.c
+++ src/Calculator/main.c
@@ -1,53 +1,94 @@
 #include <stdio.h>
+#define MAX_LIMIT 20
 
 int hasAppeared;
 int menu;
+int answer;
+
+int num1;
+int num2;
+int ans2;
 
 void calculator() {
-    
-    
-    
+    console("== Welcome to Calculator Mode! ==");
+    console("1 - Calculate");
+    console("2 - Exit");
+    scanf("%d",&answer);
+    if (answer ==1) {
+        calculate();
+    } else if (answer == 2) {
+        start();
+    } else {
+        console("Invalid Input!");
+        start();
+    }
+}
+
+void calculate() {
+    console("1st Number:");
+    scanf(%d,&num1);
+    console("2nd Number:");
+    scanf(%d,&num2);
+    int ans = num1 + num2;
+    slashN();
+    printf("%d",ans);
+    scanf(%d,&ans2);
+    if (ans2 == 1) {
+        start();
+    } else {
+        start();
+    }
+}
+
+void slashN() {
+    printf("\n");
 }
 
 void clearAll() {
     menu = 0;
     hasAppeared = 0;
+    answer = 0;
+}
+
+void console(char str[MAX_LIMIT]) {
+    slashN();
+    printf("%s", str);
 }
 
 void start() {
     
     if (hasAppeared == 0) {
         
-        printf("== Welcome to Jack's Calculator ==");
+        console("== Welcome to Jack's Calculator ==");
         
         hasAppeared = 1;
         
-        printf("1 - Calculator");
-        printf("2 - Settings");
-        printf("3 - Exit");
+        console("1 - Calculator");
+        console("2 - Credits");
+        console("3 - Exit");
         
     } else {
         
-        printf("1 - Calculator");
-        printf("2 - Settings");
-        printf("3 - Exit");
+        console("1 - Calculator");
+        console("2 - Credits");
+        console("3 - Exit");
         
     }
     
+    slashN();
     scanf("%d",&menu);
-    
     if (menu == 1) {
-        
+        calculator();
     } else if (menu == 2) {
-        
-    } else if (menu == 3) {
-        
-    } else {
-        
-        printf("Invalid Input!");
-        
+        console("== Credits ==");
+        console("Code - Colack (Jack)");
+        console("Compiler - GCC");
         start();
-        
+    } else if (menu == 3) {
+        console("Aborting...");
+    } else {
+        console("Invalid Input!");
+        start();
     }
     
 }
