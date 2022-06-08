8c719fece7a062bccc30f748853d605fa85ef698
Jack Spencer <UniformKitten281@gmail.com>
2022-06-08 08:03:27 -0700

    Update dumbscript.cjs

--- src/Dumbscriptjs/dumbscript.cjs
+++ src/Dumbscriptjs/dumbscript.cjs
@@ -6,32 +6,80 @@ function say(input) {
 
 function addThese(input1, input2) {
 
-    console.log(input1 + input2);
+    return input1 + input2;
 
 }
 
 function minusThese(input1, input2) {
 
-    console.log(input1 - input2);
+    return input1 - input2;
 
 }
 
 function timesThese(input1, input2) {
 
-    console.log(input1 * input2);
+    return input1 * input2;
 
 }
 
 function divideThese(input1, input2) {
 
-    console.log(input1 / input2);
+    return input1 / input2;
 
 }
 
-function randomNumber(from1) {
+function randomNumber(from1, plus) {
 
-    console.log(Math.floor(Math.random() * from1));    
+    return Math.floor(Math.random() * (from1 - plus)) + plus;  
 
 }
 
-module.exports = { say, addThese, minusThese, timesThese, divideThese, randomNumber };
+function randomNumberPlus(from1, from2, plus) {
+
+    return Math.floor(Math.random() * (from2 - from1)) + from1 + plus;
+
+}
+
+function randomNumberMinus(from1, from2, minus) {
+
+    return Math.floor(Math.random() * (from2 - from1)) + from1 - minus;
+
+}
+
+function randomNumberTimes(from1, from2, times) {
+
+    return Math.floor(Math.random() * (from2 - from1)) + from1 * times;
+
+}
+
+function randomNumberDivide(from1, from2, divide) {
+
+    return Math.floor(Math.random() * (from2 - from1)) + from1 / divide;
+
+}
+
+function powers(base, power) {
+
+    return Math.pow(base, power);
+
+}
+
+function squareRoot(input) {
+
+    return Math.sqrt(input);
+
+}
+
+function cubeRoot(input) {
+
+    return Math.cbrt(input);
+
+}
+
+function square(input) {
+
+    return input * input;
+
+}
+
+module.exports = { say, addThese, minusThese, timesThese, divideThese, randomNumber, randomNumberPlus, randomNumberMinus, randomNumberTimes, randomNumberDivide, powers, squareRoot, cubeRoot, square };
