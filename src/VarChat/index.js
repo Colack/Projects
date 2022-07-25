0ba6204ede54c1a915ae194e0fc913de48fafdcc
Jack Spencer <UniformKitten281@gmail.com>
2022-07-24 17:00:22 -0700

    Create index.js

---
+++ src/VarChat/index.js
@@ -0,0 +1,23 @@
+/*
+
+  Completed By Colack
+  
+  Comments:
+  
+    This is varSite, except its just for chatting!
+    
+    Oh yeah, I guess I'm doing code.org Coding again :)
+
+*/
+
+onAppStart();
+
+
+// Essentially int Main
+function onAppStart() {
+  
+  setScreen("Index");
+  
+  
+  
+}
